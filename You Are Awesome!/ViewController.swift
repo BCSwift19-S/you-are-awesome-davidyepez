8a3ce83be49edaf7695cbb10fd84541b15c609ca
David Yepez <yepezd@bc.edu>
2019-01-23 13:46:08 -0500

    2.10 Complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
     
     
     override func viewDidLoad() {
@@ -17,31 +18,43 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let message1 = "You are Awesome!"
-        let message2 = "You are Great!"
-        let message3 = "You are Amazing!"
         
         
-        if messageLabel.text == message1{
-            messageLabel.text = message2
-        } else if messageLabel.text == message2 {
-            messageLabel.text = message3
+        let messages = ["You Are Awesome!",
+                        "You Are Great!",
+                        "You Are Fantastic!",
+                        "When the genius bar needs help they call you!",
+                        "You brighten my day!",
+                        "You make me smile!",
+                        "You are da bomb!",
+                        "Hey, fabulous!"]
+
+        messageLabel.text = messages[index]
+       
+        
+        if index == messages.count - 1 {
+            index = 0
         } else {
-            messageLabel.text = message1
+             index = index + 1
         }
-        
-        
-        
-        
-        
-        
-        
         
         
     }
     
-    
-    
 }
 
 
+/*       let message1 = "You are Awesome!"
+ let message2 = "You are Great!"
+ let message3 = "You are Amazing!"
+ 
+ 
+ if messageLabel.text == message1{
+ messageLabel.text = message2
+ } else if messageLabel.text == message2 {
+ messageLabel.text = message3
+ } else {
+ messageLabel.text = message1
+ }
+ 
+ */
